00fd549b8347a5f0834d9ea83c5956f2a40aa641
Jhonatan Rico <xricoxdrumsx@gmail.com>
2022-11-02 23:42:41 -0500

    Added navigation bar to use on public or auth views

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,15 +1,14 @@
 <template>
-  <q-layout view="hHh lpR fFf">
-
-    <q-page-container>
+  <q-layout view="hHh lpR fFf" class="background main-layout">
+    
+    <q-page-container class="container">
+      <navigation-bar />
       <router-view />
     </q-page-container>
 
-    <q-footer class="bg-transparent text-dark" height-hint="50px">
-      <q-toolbar class="justify-center">
-          <div>Contact Book</div>
-      </q-toolbar>
-    </q-footer>
-
   </q-layout>
 </template>
+<script lang="ts" setup>
+import NavigationBar from '../components/NavigationBar.vue';
+components: { NavigationBar }
+</script>
